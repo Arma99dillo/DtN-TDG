49cd13026ccebc04b4dd0b464e6ccf6c064c93fb
Armando Maria Monforte <142396019+Arma99dillo@users.noreply.github.com>
2025-05-27 11:28:55 +0200

    Add files via upload

---
+++ ThetaConvEps10.m
@@ -0,0 +1,77 @@
+% DtN-TDG solver for Helmholtz equation on periodic grating 
+% Convergence test
+
+close all; addpath quadtriangle; addpath src
+
+%-----------------------------------
+%Parameters definition
+%-----------------------------------
+%Problem parameters
+param.K=5; %wavenumber
+theta_0=-1.441203660687987;
+
+%Discretization parameters
+param.h=0.5; %mesh width
+param.alpha=1/2; param.beta=1/2; param.delta=1/2; %TDG flux coefficients 
+param.M=20; %number of Fourier modes
+
+param.nd=30; %number of plane wave directions
+%define the plane wave direction vectors
+param.d=zeros(2,param.nd);
+for l=1:param.nd
+    param.d(:,l)=[cos((2*pi*l)/param.nd); sin((2*pi*l)/param.nd)];
+end
+
+%-----------------------------------
+%Mesh definition
+%-----------------------------------
+%Select domain - only double and triple rectangle available
+domain = 'triple_rectangle_eps10';
+
+disp(['Theta-convergence test for epsilon=10, with k=', num2str(param.K)...
+    ', p=', num2str(param.nd), ', h=', num2str(param.h)])
+
+%-----------------------------------
+%Cycle on theta
+%-----------------------------------
+%basis functions and derivatives
+phi = @(x1,x2,d,k) exp(1i*k.*(x1.*d(1)+x2.*d(2)));
+grad_phi = @(x1,x2,d,k) 1i*k.*d.*exp(1i*k.*(x1.*d(1)+x2.*d(2)));
+
+thetavect=theta_0-1e-4:1e-5:theta_0+1e-4;
+L2Error=zeros(size(thetavect));   %error vectors
+for v=1:size(thetavect,2) %cycle on theta
+    
+    param.theta=thetavect(v); %incident angle
+    param.alp=param.K*cos(param.theta); %quasi-periodicity parameter
+
+    %generate mesh and compute exact solution
+    [mesh,param,uex,uexdx,uexdy] = GenerateMeshSol(param,domain);
+    
+    disp([ 'Linear system assembly for incident angle theta=', num2str(param.theta) ])
+    A = MatrixDtNTDG(mesh,param); %system matrix
+    b = rhsDtNTDG(mesh,param); %system rhs
+    u=A\b; %solve the system
+
+    %Error computation
+    [err2] = SolErr(mesh,param,u,phi,grad_phi,uex,uexdx,uexdy);
+    L2Error(v) = err2;
+
+end
+
+
+%-----------------------------------
+%Convergence plot
+%-----------------------------------
+figure()
+semilogy(thetavect,L2Error,'*-','LineWidth',1.2); grid
+xlim([theta_0-1.1e-4 theta_0+1.1e-4])
+LL = legend('$L^2$ error','FontSize', 14);
+set(LL, 'Interpreter', 'latex');
+a = get(gca,'XTickLabel');
+set(gca,'XTickLabel',a,'fontsize',14,'TickLabelInterpreter', 'latex')
+xticks([theta_0-1e-4, theta_0, theta_0+1e-4])
+a = get(gca,'YTickLabel');
+set(gca,'YTickLabel',a,'fontsize',14,'TickLabelInterpreter', 'latex')
+xlabel('Incident angle','FontSize',18, 'Interpreter','latex')
+ylabel('Error','FontSize',18, 'Interpreter','latex')
---
+++ pConvTwoFlatNoabs.m
@@ -0,0 +1,77 @@
+% DtN-TDG solver for Helmholtz equation on periodic grating 
+% Convergence test
+
+close all; addpath quadtriangle; addpath src
+
+%-----------------------------------
+%Parameters definition
+%-----------------------------------
+%Problem parameters
+param.K=5; %wavenumber
+param.theta=-pi/3; %incident angle
+param.alp=param.K*cos(param.theta); %quasi-periodicity parameter
+
+%Discretization parameters
+param.h=1.5; %mesh width
+param.alpha=1/2; param.beta=1/2; param.delta=1/2; %TDG flux coefficients 
+param.M=20; %number of Fourier modes
+
+
+%-----------------------------------
+%Mesh definition
+%-----------------------------------
+%Select domain - only double and triple rectangle available
+domain = 'double_rectangle_no_abs';
+%generate mesh and compute exact solution
+[mesh,param,uex,uexdx,uexdy] = GenerateMeshSol(param,domain);
+
+disp(['DtN-TDG p-convergence test on the domain with 2 flat interfaces and no absorption: k=',...
+    num2str(param.K), ', h=', num2str(param.h)])
+
+if param.K.*param.L/2 > param.M
+    warning('The value of M is too small, consider increasing for better stability')
+end
+
+%-----------------------------------
+%Cycle on number of directions
+%-----------------------------------
+%basis functions and derivatives
+phi = @(x1,x2,d,k) exp(1i*k.*(x1.*d(1)+x2.*d(2)));
+grad_phi = @(x1,x2,d,k) 1i*k.*d.*exp(1i*k.*(x1.*d(1)+x2.*d(2)));
+
+ni=3; nf=30; %min and max value
+L2Error=zeros(nf-ni+1,1); H1Error=zeros(nf-ni+1,1); v=1; %error vectors
+for nd=ni:nf %cycle on number of PW directions
+
+    param.nd=nd; %number of plane wave directions
+    %define the plane wave direction vectors
+    param.d=zeros(2,param.nd); 
+    for l=1:param.nd
+        param.d(:,l)=[cos((2*pi*l)/param.nd); sin((2*pi*l)/param.nd)];
+    end
+
+    disp(['Linear system assembly: p=', num2str(param.nd),...
+        ', ', num2str(param.nd.*size(mesh.t,1)), ' basis functions' ])
+    A = MatrixDtNTDG(mesh,param); %system matrix
+    b = rhsDtNTDG(mesh,param); %system rhs
+    u=A\b; %solve the system
+
+    %Error computation
+    [err2,err1] = SolErr(mesh,param,u,phi,grad_phi,uex,uexdx,uexdy);
+    L2Error(v) = err2;
+    H1Error(v) = err1;
+    v=v+1;
+end
+
+
+%-----------------------------------
+%Convergence plot
+%-----------------------------------
+PlotConvergence(ni,nf,L2Error,H1Error,5:5:nf);
+
+
+%-----------------------------------
+%Solution and error plot
+%-----------------------------------
+disp('Plotting numerical solution and error')
+PlotErrorLog(mesh,param,u,phi,uex);
